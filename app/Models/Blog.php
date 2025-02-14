2cc71238ad3dded11bb05651e93a28b51c4b5730
Ine <ine@inetjuhhh.nl>
2025-02-14 11:00:51 +0100

    build models

---
+++ app/Models/Blog.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    protected $table = 'blogs';
+    protected $guarded = [];
+
+    public function placed_by()
+    {
+        return $this->belongsTo(User::class, 'placed_by');
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+}
