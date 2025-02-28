83c8012d6eccf07d0804a26e8baf7c65524b3321
Ine <ine@inetjuhhh.nl>
2025-02-28 11:44:59 +0100

    add custom tailwind classes for filament

--- resources/views/tiptapblocks/preview/gallery.blade.php
+++ resources/views/tiptapblocks/preview/gallery.blade.php
@@ -24,8 +24,8 @@
     @if($count > 0)
         <div class="grid grid-cols-{{ $columns }} gap-4 mt-4">
             @foreach($images as $image)
-                <div class="overflow-hidden rounded-md shadow-md border-2">
-                    <img src="{{ asset('storage/' . $image) }}" class="@if($count == 1) w-2/5 @else w-full @endif h-auto object-cover rounded-md">
+                <div class="w-24 overflow-hidden rounded-md shadow-md">
+                    <img src="{{ asset('storage/' . $image) }}" class="w-24 rounded-md">
                 </div>
             @endforeach
         </div>
